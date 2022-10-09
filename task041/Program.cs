ee70da630010730189ff3e5808f2110be1378634
Yuri Malyshev <team21_07@list.ru>
2022-10-09 19:08:59 +0300

    task041 complete

---
+++ task041/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+int[] numersArray = UserEnterNumersConsol();
+ResultOutput(numersArray);
+
+int[] UserEnterNumersConsol()
+{
+    Console.Write("Введите общее количество чисел ");
+    int sise;
+    sise = int.Parse(Console.ReadLine());
+    int[] userEnterNumersArray = new int[sise];
+    for (int i = 0, j = 1; j <= sise; i++, j++)
+    {
+        Console.Write($"Введите число {j} из {sise} : ");
+        userEnterNumersArray[i] = int.Parse(Console.ReadLine());
+    }
+    return (userEnterNumersArray);
+}
+
+void ResultOutput(int[] numerArray)
+{
+    int sumPositivNums = 0;
+    for (int i = 0; i < numerArray.Length; i++)
+    {
+        if (numerArray[i] > 0) { sumPositivNums++; }
+    }
+    Console.Write($"Количество чисел больше 0 = {sumPositivNums}");
+}
