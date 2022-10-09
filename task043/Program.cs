20b10f069639ba4c9bf7a4e59924bc2a18c595ef
Yuri Malyshev <team21_07@list.ru>
2022-10-09 20:54:09 +0300

    tasks complete

---
+++ task043/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, 
+//y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Clear();
+double[] numersArray = UserEnterNumersConsol();
+ResultOutput(numersArray);
+
+double[] UserEnterNumersConsol()
+{
+    double[] userEnterNumersArray = new double[4];
+    Console.Write("Введите число b1: ");
+    userEnterNumersArray[0] = double.Parse(Console.ReadLine());
+    Console.Write($"Введите число k1: ");
+    userEnterNumersArray[1] = double.Parse(Console.ReadLine());
+    Console.Write($"Введите число b2: ");
+    userEnterNumersArray[2] = double.Parse(Console.ReadLine());
+    Console.Write($"Введите число k2: ");
+    userEnterNumersArray[3] = double.Parse(Console.ReadLine());
+    return (userEnterNumersArray);
+}
+
+void ResultOutput(double[] numerArray)
+{
+    double[] xyArray = new double[2];
+    xyArray[0] = (numerArray[2] - numerArray[0]) / (numerArray[1] - numerArray[3]);
+    xyArray[1] = (numerArray[1] * xyArray[0]) + numerArray[0];
+    Console.Write($"Точка пересечения прямых = ({xyArray[0]};{xyArray[1]})");
+}
---
+++ task045/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 45: Напишите программу, 
+//которая будет создавать копию заданного массива 
+//с помощью поэлементного копирования.
+
+Console.Clear();
+int[] numersArray = UserEnterNumersConsol();
+int[] copyNumersArray = CopyArray(numersArray);
+ResultOutput(copyNumersArray);
+
+int[] UserEnterNumersConsol()
+{
+    Console.Write("Введите общее количество чисел массива ");
+    int sise;
+    sise = int.Parse(Console.ReadLine());
+    int[] userEnterNumersArray = new int[sise];
+    for (int i = 0, j = 1; j <= sise; i++, j++)
+    {
+        Console.Write($"Введите число {j} из {sise} : ");
+        userEnterNumersArray[i] = int.Parse(Console.ReadLine());
+    }
+    return (userEnterNumersArray);
+}
+
+int[] CopyArray(int[] numArray)
+{
+    int[] copyArr = new int[numArray.Length];
+    for (int i = 0; i < numArray.Length; i++)
+    {
+        copyArr[i] = numArray[i];
+    }
+    return (copyArr);
+}
+
+void ResultOutput(int[] numerArray)
+{
+    Console.Write("Поэлементная копия массива = ( ");
+    for (int i = 0; i < numerArray.Length; i++)
+    {
+        Console.Write($"{numerArray[i]} ");
+    }
+    Console.Write(")");
+}
